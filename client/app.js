5bede6d603b8da728dd44496d4242732e4900a1b
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2016-07-09 22:47:09 -0700

    working client side compilation (not hooked up)

--- client/app.js
+++ client/app.js
@@ -1,19 +1,11 @@
 'use strict'
 
 import React from 'react';
-import { Router, Route } from 'react-router';
 import { render } from 'react-dom';
-import Layout from './components/layout';
-import Home from './components/home';
+import router from '../router';
 
 render(
-  (
-    <Router>
-      <Route component={Layout}>
-        <Route path="/" component={Home} />
-      </Route>
-    </Router>
-  ),
+  router,
   document.getElementById('app')
 );
 
